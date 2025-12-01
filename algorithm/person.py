bf47845544ce31e2e829891b61092af022d99453
Wxrui <wxr19961114@163.com>
2025-12-01 19:22:47 +0800

    初始化了算法模块

--- algorithm/person.py
+++ algorithm/person.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 # developTime: 2025/12/1 19:12
 class Person:
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def eat(self):
+        print("%s在吃饭" % self.name)
+
